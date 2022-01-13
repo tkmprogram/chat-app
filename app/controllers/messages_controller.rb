e5827cc0e31d79ae2ff7490f87f0c9e4999e7a0a
tkmprogram <takumi.kojo.2@gmail.com>
2022-01-13 17:02:13 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,6 +21,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :imarge).merge(user_id: current_user.id)
   end
 end
